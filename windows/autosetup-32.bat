033d6af965f8dca806db5ea865e93de6005b9a8b
Dom <dominic@dom.gs>
2015-07-27 15:10:40 +0100

    Win autosetup fix for new NSSM exe

--- windows/autosetup-32.bat
+++ windows/autosetup-32.bat
@@ -3,15 +3,15 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`chdir`) DO (
 SET dir=%%F
 )
 
-"%dir%\windows\win32\nssm.exe" install "GoDaddy Dynamic DNS" "%dir%\windows\dnsupdate.bat"
-"%dir%\windows\win32\nssm.exe" stop "GoDaddy Dynamic DNS"
+"%dir%\windows\nssm-32.exe" install "GoDaddy Dynamic DNS" "%dir%\windows\dnsupdate.bat"
+"%dir%\windows\nssm-32.exe" stop "GoDaddy Dynamic DNS"
 
-"%dir%\windows\win32\nssm.exe" set "GoDaddy Dynamic DNS" Application "%dir%\windows\dnsupdate.bat"
-"%dir%\windows\win32\nssm.exe" set "GoDaddy Dynamic DNS" AppParameters "%dir%\bin\godaddy.py"
-"%dir%\windows\win32\nssm.exe" set "GoDaddy Dynamic DNS" AppDirectory "%dir%\bin"
-"%dir%\windows\win32\nssm.exe" set "GoDaddy Dynamic DNS" Start SERVICE_DELAYED_AUTO_START
+"%dir%\windows\nssm-32.exe" set "GoDaddy Dynamic DNS" Application "%dir%\windows\dnsupdate.bat"
+"%dir%\windows\nssm-32.exe" set "GoDaddy Dynamic DNS" AppParameters "%dir%\bin\godaddy.py"
+"%dir%\windows\nssm-32.exe" set "GoDaddy Dynamic DNS" AppDirectory "%dir%\bin"
+"%dir%\windows\nssm-32.exe" set "GoDaddy Dynamic DNS" Start SERVICE_DELAYED_AUTO_START
 
-"%dir%\windows\win32\nssm.exe" start "GoDaddy Dynamic DNS"
+"%dir%\windows\nssm-32.exe" start "GoDaddy Dynamic DNS"
 
 echo Setup Done... 
 echo.
--- windows/autosetup-64.bat
+++ windows/autosetup-64.bat
@@ -3,15 +3,15 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`chdir`) DO (
 SET dir=%%F
 )
 
-"%dir%\windows\win64\nssm.exe" install "GoDaddy Dynamic DNS" "%dir%\windows\dnsupdate.bat"
-"%dir%\windows\win64\nssm.exe" stop "GoDaddy Dynamic DNS"
+"%dir%\windows\nssm-64.exe" install "GoDaddy Dynamic DNS" "%dir%\windows\dnsupdate.bat"
+"%dir%\windows\nssm-64.exe" stop "GoDaddy Dynamic DNS"
 
-"%dir%\windows\win64\nssm.exe" set "GoDaddy Dynamic DNS" Application "%dir%\windows\dnsupdate.bat"
-"%dir%\windows\win64\nssm.exe" set "GoDaddy Dynamic DNS" AppParameters "%dir%\bin\godaddy.py"
-"%dir%\windows\win64\nssm.exe" set "GoDaddy Dynamic DNS" AppDirectory "%dir%\bin"
-"%dir%\windows\win64\nssm.exe" set "GoDaddy Dynamic DNS" Start SERVICE_DELAYED_AUTO_START
+"%dir%\windows\nssm-64.exe" set "GoDaddy Dynamic DNS" Application "%dir%\windows\dnsupdate.bat"
+"%dir%\windows\nssm-64.exe" set "GoDaddy Dynamic DNS" AppParameters "%dir%\bin\godaddy.py"
+"%dir%\windows\nssm-64.exe" set "GoDaddy Dynamic DNS" AppDirectory "%dir%\bin"
+"%dir%\windows\nssm-64.exe" set "GoDaddy Dynamic DNS" Start SERVICE_DELAYED_AUTO_START
 
-"%dir%\windows\win64\nssm.exe" start "GoDaddy Dynamic DNS"
+"%dir%\windows\nssm-64.exe" start "GoDaddy Dynamic DNS"
 
 echo Setup Done... 
 echo.

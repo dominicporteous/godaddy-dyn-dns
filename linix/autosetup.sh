8dcfd8e0ebfad8baabb397c4b20a667e3dad61fb
dominicporteous <dominic@dom.gs>
2015-08-01 05:17:50 +0100

    Update autosetup.sh
    Fix for python deps

--- linix/autosetup.sh
+++ linix/autosetup.sh
@@ -5,6 +5,10 @@ sudo mkdir -r /var/log/gd-dyn-dns
 sudo cp gd-dyn-dns /etc/init.d/gd-dyn-dns
 sudo cp ./bin /etc/gd-dyn-dns
 
+
+pip install requests
+pip install tldextract
+
 sudo chkconfig --add gd-dyn-dns
 sudo chkconfig gd-dyn-dns on
 
